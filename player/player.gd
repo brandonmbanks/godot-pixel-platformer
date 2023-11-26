e6f0477e5abc6bcb3bd819d98ce36a0379af9f56
Brandon Banks <10030196+brandonmbanks@users.noreply.github.com>
2023-11-26 12:49:48 -0500

    add jump animations and player skins

--- player/player.gd
+++ player/player.gd
@@ -12,18 +12,12 @@ extends CharacterBody2D
 
 var jumps_remaining: int = NUM_JUMPS
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# constantly apply gravity
 	apply_gravity()
 	
 	var input = Vector2.ZERO
 	input.x = Input.get_axis("ui_left", "ui_right")
-	
-	# flip sprite direction
-	if input.x > 0:
-		animated_sprite_2d.flip_h = true
-	elif input.x < 0:
-		animated_sprite_2d.flip_h = false
 
 	# movement
 	if input.x == 0:
@@ -39,19 +33,35 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		jumps_remaining = NUM_JUMPS
 	else:
+		animated_sprite_2d.play("jump")
 		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
 			velocity.y = JUMP_RELEASE_FORCE
 		
 		if velocity.y > 0:
 			velocity.y += GRAVITY * 1.5
 
+	var was_in_air = not is_on_floor()
 	move_and_slide()
+	var just_landed = is_on_floor() and was_in_air
+	# if we just landed we want to set the animation to te 2nd frame of run
+	if just_landed:
+		animated_sprite_2d.play("run")
+		animated_sprite_2d.set_frame(1)
 
 func apply_gravity():
 	velocity.y += GRAVITY
+	velocity.y = min(velocity.y, 300)
 
 func apply_friction():
+	animated_sprite_2d.play("idle")
 	velocity.x = move_toward(velocity.x, 0, FRICTION)
 
 func apply_acceleration(input_x: float):
+	# flip sprite direction
+	if input_x > 0:
+		animated_sprite_2d.flip_h = true
+	elif input_x < 0:
+		animated_sprite_2d.flip_h = false
+
+	animated_sprite_2d.play("run")
 	velocity.x = move_toward(velocity.x, MAX_SPEED * input_x, ACCELERATION)
