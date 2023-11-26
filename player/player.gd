4af0b905567f41ccabcc1aee20166fcd50918afc
Brandon Banks <10030196+brandonmbanks@users.noreply.github.com>
2023-11-25 22:04:20 -0500

    add double jump

--- player/player.gd
+++ player/player.gd
@@ -8,27 +8,39 @@ extends CharacterBody2D
 @export var MAX_SPEED: int = 75
 @export var JUMP_FORCE: int = -140
 @export var JUMP_RELEASE_FORCE: int = -70
+@export var NUM_JUMPS: int = 2
 
 func _physics_process(delta: float) -> void:
+	# constantly apply gravity
 	apply_gravity()
 	
 	var input = Vector2.ZERO
 	input.x = Input.get_axis("ui_left", "ui_right")
 	
+	# flip sprite direction
 	if input.x > 0:
 		animated_sprite_2d.flip_h = true
 	elif input.x < 0:
 		animated_sprite_2d.flip_h = false
 
+	# movement
 	if input.x == 0:
 		apply_friction()
 	else:
 		apply_acceleration(input.x)
 
+	# jump
+	# only allow jump if on floor
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
+		NUM_JUMPS = 2
+		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_FORCE
+			NUM_JUMPS -= 1
 	else:
+		if Input.is_action_just_pressed("ui_up") and NUM_JUMPS > 0:
+			velocity.y = JUMP_FORCE
+			NUM_JUMPS -= 1
+
 		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
 			velocity.y = JUMP_RELEASE_FORCE
 		
